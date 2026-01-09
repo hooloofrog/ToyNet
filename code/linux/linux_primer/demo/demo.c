cb0296a739174ba356aaa198331c3a4e2b447120
hooloofrog <52738474+hooloofrog@users.noreply.github.com>
2026-01-09 18:39:30 +0800

    添加配置修改

--- code/linux/linux_primer/demo/demo.c
+++ code/linux/linux_primer/demo/demo.c
@@ -1,16 +1,14 @@
 
 #include <stdio.h>
-#include <unistd.h>
+#include <time.h>
 
-int main()
+void Print(int sum)
 {
-    int cnt = 10;
-    while (cnt)
-    {
-        printf("剩余时间:%2d\r", cnt);  // 利用\r回车来实现同一个像素点，显示多个数字，这样看起来就像倒计时
-        fflush(stdout);                // 每次刷新一下缓冲区，立马显示数据
-        cnt--;
-        sleep(1);
-    }
+    long long time_stamp = time(NULL);
+}
+
+int main ()
+{
+    
     return 0;
 }
